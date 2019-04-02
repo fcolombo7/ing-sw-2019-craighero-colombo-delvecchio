60d1a58ec2c14b617a8edecfe8bcb2873ed7f123
giovannidv8 <giovanni.delvecchio@mail.polimi.it>
2019-04-03 00:08:59 +0200

    Game class added

---
+++ src/main/java/it/polimi/ingsw/model/Game.java
@@ -0,0 +1,68 @@
+package it.polimi.ingsw.model;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class Game {
+    private HashMap<String, Requirement>;
+    private String weaponFilename;
+    private String powerupFilename;
+    private String gameSettingsFilename;
+    private String ammoFilename;
+    private Player currentPlayer;
+    private GameSatus status;
+    private Turn currentTurn;
+    private List<AmmoTile> ammoTileDeck;
+    private List<Weapon> weaponDeck;
+    private List<Powerup> powerupDeck;
+    private List<Player> players;
+    private GameBoard gameBoard;
+
+    public Game(String weaponFilename, String powerupFilename, String gameSettingsFilename, String ammoFilename){}
+
+    public void setStatus(GameStatus status){}
+
+    public void addPlayers(String nickname, String motto){}
+
+    public void createTurn(Game game){}
+
+    public List<String[]> calcWinner(){
+        return winners;
+    }
+
+    public void initWeapon(Weapon weapon){}
+
+    public void initPowerup(Powerup powerup){}
+
+    private void loadWeapons(){}
+
+    private void loadPowerups(){}
+
+    private void loadAmmos(){}
+
+    private void loadSettings(){}
+
+    public Requirement getRequirement(String requirement){
+        return req;
+    }
+
+    public Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public GameSatus getStatus() {
+        return status;
+    }
+
+    public Turn getCurrentTurn() {
+        return currentTurn;
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public GameBoard getGameBoard() {
+        return gameBoard;
+    }
+}
