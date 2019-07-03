c9cd0c44563e9532b49333c56c29b2c9b7adcea1
Filippo Colombo <filippo7.colombo@mail.polimi.it>
2019-07-03 18:29:32 +0200

    Recover BUG

---
+++ src/main/java/it/polimi/ingsw/tests/TestGson.java
@@ -0,0 +1,52 @@
+package it.polimi.ingsw.tests;
+
+import com.google.gson.Gson;
+import it.polimi.ingsw.model.*;
+import it.polimi.ingsw.network.controller.messages.LoginMessage;
+import it.polimi.ingsw.network.controller.messages.matchanswer.BoardPreferenceAnswer;
+import it.polimi.ingsw.network.controller.messages.matchanswer.MatchAnswer;
+import it.polimi.ingsw.network.controller.messages.matchmessages.BoardUpdateMessage;
+import it.polimi.ingsw.network.controller.messages.matchmessages.MatchMessage;
+
+public class TestGson {
+    public static void main(String[] args){
+        Gson gson = new Gson();
+        LoginMessage lgn=new LoginMessage("text","asd");
+        System.out.println(gson.toJson(lgn));
+
+        MatchAnswer msg=new BoardPreferenceAnswer("nickname",1);
+        System.out.println(gson.toJson(msg));
+        Game game=new Game();
+        game.addPlayer(new Player("Ciao","",true));
+        game.addPlayer(new Player("Ciaooo","",false));
+        game.setGameBoard(1);
+        MatchMessage m=new BoardUpdateMessage(game.getGameBoard());
+        //Gson gson1=new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+        System.out.println(gson.toJson(m));
+
+/*
+        ArrayList<Weapon> arrayList=new ArrayList<>();
+        Weapon weapon=new Weapon("0","","src/main/Resources/weapons/vulcanizzatore.xml");
+        Weapon weapon1=new Weapon("1","","src/main/Resources/weapons/vulcanizzatore.xml");
+        Weapon weapon2=new Weapon("2","","src/main/Resources/weapons/vulcanizzatore.xml");
+        arrayList.add(weapon);
+        System.out.println(gson.toJson(arrayList));
+
+        Square s= new WeaponSquare(RoomColor.RED,new boolean[]{true,false,true,false},new int[]{0,0});
+        ((WeaponSquare)s).addWeapon(weapon);
+        ((WeaponSquare)s).addWeapon(weapon1);
+        ((WeaponSquare)s).addWeapon(weapon2);
+
+        Square s2= new WeaponSquare(RoomColor.RED,new boolean[]{true,false,true,false},new int[]{0,0});
+        ((WeaponSquare)s2).addWeapon(weapon);
+        ((WeaponSquare)s2).addWeapon(weapon1);
+        ((WeaponSquare)s2).addWeapon(weapon2);
+
+        Square[][] mat=new Square[][]{{null,null,null},{null,null,null}};
+        mat[0][1]=s;
+        mat[1][0]=s2;
+        System.out.println(gson.toJson(mat));
+
+ */
+    }
+}
